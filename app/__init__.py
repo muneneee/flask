f7a6b74542ced5ffaafa58ad89708a1e4dabf806
[muneneee] <[lukekevin39@gmail.com]>
2020-04-26 21:02:58 +0300

    created the base html

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from .config import DevConfig
-from flask_bootstarp import Bootstrap
+from flask_bootstrap import Bootstrap
 
 #init app
 app = Flask(__name__,instance_relative_config = True)
